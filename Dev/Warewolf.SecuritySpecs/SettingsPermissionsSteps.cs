2495f1c0a87e71f0700bf0127982fb46d01ddebd
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-11-11 15:18:02 +0200

    compile

--- Dev/Warewolf.SecuritySpecs/SettingsPermissionsSteps.cs
+++ Dev/Warewolf.SecuritySpecs/SettingsPermissionsSteps.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Security.Principal;
 using System.Threading;
